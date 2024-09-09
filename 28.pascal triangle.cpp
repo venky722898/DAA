bcca41cf2640bd1d6ebe6e651e755f0c79bc0bc8
VENKATESWARI620 <132537878+venky722898@users.noreply.github.com>
2024-09-09 12:45:38 +0530

    Add files via upload

---
+++ 28.pascal triangle.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printPascal(int n) {
+    for (int line = 1; line <= n; line++) {
+        // Print leading spaces for alignment
+        for (int k = 0; k < n - line; k++) {
+            printf(" ");
+        }
+
+        int C = 1; // First element is always 1
+        for (int i = 1; i <= line; i++) {
+            printf("%d ", C);
+            C = C * (line - i) / i;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+	 printf("K.Venkateswari (192210620)\n");
+    int n;
+    printf("Enter the number of lines for Pascal's Triangle: ");
+    scanf("%d", &n);
+    printPascal(n);
+    return 0;
+}
