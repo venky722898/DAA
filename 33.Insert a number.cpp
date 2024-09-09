21946068e94ff3964910ab4f3dea9cdb18e80545
VENKATESWARI620 <132537878+venky722898@users.noreply.github.com>
2024-09-09 23:02:11 +0530

    Add files via upload

---
+++ 33.Insert a number.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void insertNumber(int arr[], int n, int num, int pos) {
+    for (int i = n; i >= pos; i--)
+        arr[i] = arr[i - 1];
+    arr[pos - 1] = num;
+}
+
+int main() {
+    int arr[100], n, num, pos;
+     printf("K.Venkateswari (192210620)\n");
+
+    // Get array size from user
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    // Get array elements from user
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Get the number to be inserted from user
+    printf("Enter the number to be inserted: ");
+    scanf("%d", &num);
+
+    // Get the position at which the number should be inserted from user
+    printf("Enter the position at which to insert the number: ");
+    scanf("%d", &pos);
+
+    // Insert the number at the specified position
+    insertNumber(arr, n, num, pos);
+    n++; // Increment the size of the array after insertion
+
+    // Print the student information
+
+
+    // Print the array after insertion
+    printf("Array after insertion: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
